73e208f4dda80a5154dd2b18812087443115d5be
hrdtbs <harada.tsubasa.0422@gmail.com>
2023-08-07 02:23:33 +0900

    feat: simplify posts page

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,18 @@
-import BlogCard from "@/components/blog-card";
-import BlogPageLayout from "@/components/layout/blog-page-layout";
-import Link from "@/components/link";
-import PostPaginator from "@/components/post-paginator";
 import RenderPosts from "@/components/render-posts";
 import { allSortedBlogs } from "@/lib/contentlayer";
-import config from "@/lib/siteConfig";
-import { cn, generateCommonMeta } from "@/lib/utils";
+import { generateCommonMeta } from "@/lib/utils";
 import { Metadata } from "next";
 
 export const metadata: Metadata = generateCommonMeta({
   title: "Blog",
-  description: "Latest blog posts",
+  description: "Blog posts",
   image: "/api/og",
 });
 
 const Page = () => {
   const blogs = [...allSortedBlogs];
 
-  return (
-    <main>
-      <BlogPageLayout title="All Blog Posts">
-        <RenderPosts posts={blogs} />
-      </BlogPageLayout>
-    </main>
-  );
+  return <RenderPosts posts={blogs} />;
 };
 
 export default Page;
