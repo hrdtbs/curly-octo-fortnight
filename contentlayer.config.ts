3b965c814068c809d67417f03414f8b9ab559ce2
hrdtbs <harada.tsubasa.0422@gmail.com>
2023-08-07 12:57:12 +0900

    chore: remove remarkNormalizeHeadings

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -4,14 +4,13 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
-import remarkNormalizeHeadings from "./lib/mdx/remark-normalize-headings";
 import BlogPost from "./schema/contentlayer/blog-post";
 
 export default makeSource({
   contentDirPath: "posts",
   documentTypes: [BlogPost],
   mdx: {
-    remarkPlugins: [remarkGfm, flattenImageParagraphs, remarkNormalizeHeadings],
+    remarkPlugins: [remarkGfm, flattenImageParagraphs],
     rehypePlugins: [
       rehypeSlug,
       [
