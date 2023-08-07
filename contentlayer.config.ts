ea850f84380650c48f5de54ac55d4e4a64582467
hrdtbs <harada.tsubasa.0422@gmail.com>
2023-08-07 13:05:15 +0900

    chore: combine contentlayer codes into one file

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -4,7 +4,76 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
-import BlogPost from "./schema/contentlayer/blog-post";
+import { defineDocumentType } from "contentlayer/source-files";
+import GithubSlugger from "github-slugger";
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { toString } from "mdast-util-to-string";
+import { visit } from "unist-util-visit";
+
+type Headings = Array<{ depth: number; text: string; slug: string }>;
+
+function extractHeadings(rawPostBody: string) {
+  const slugger = new GithubSlugger();
+  const tree = fromMarkdown(rawPostBody);
+  const headings: Headings = [];
+  visit(tree, "heading", (node) => {
+    const text = toString(node);
+    headings.push({
+      depth: node.depth,
+      slug: slugger.slug(text),
+      text,
+    });
+  });
+  slugger.reset();
+  return headings;
+}
+
+const BlogPost = defineDocumentType(() => ({
+  name: "BlogPost",
+  contentType: "mdx",
+  filePathPattern: "*.md",
+  fields: {
+    title: {
+      type: "string",
+      description: "The title of the post",
+      required: true,
+    },
+    description: {
+      type: "string",
+      description: "Short description of the post",
+      required: true,
+    },
+    date: {
+      type: "date",
+      description: "The date when the post was published",
+      required: true,
+    },
+    lastmod: {
+      type: "date",
+      description: "The date when the post was last modified",
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+      description: "Post tags",
+    },
+    image: {
+      type: "string",
+      description: "Heading image",
+    },
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (post) => post._raw.flattenedPath,
+    },
+    headings: {
+      type: "json",
+      description: "All headings from the post",
+      resolve: (post) => extractHeadings(post.body.raw),
+    },
+  },
+}));
 
 export default makeSource({
   contentDirPath: "posts",
